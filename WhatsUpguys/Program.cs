fda6136274ca6fd5fa8848cb0a281b0f319ef86e
Sam Grantham 🧙 <sagran@microsoft.com>
2019-01-29 13:46:24 -0600

    adding things

---
+++ WhatsUpguys/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatsUpguys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's Up Guys");
+        }
+    }
+}
